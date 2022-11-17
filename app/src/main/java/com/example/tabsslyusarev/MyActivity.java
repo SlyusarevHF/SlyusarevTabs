adb8389d9da83bd9fb030ab8328b4392eb810242
SlyusarevHF <nik.slyusarev.2019@mail.ru>
2022-11-17 17:20:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tabsslyusarev/MyActivity.java
@@ -0,0 +1,16 @@
+package com.example.tabsslyusarev;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MyActivity extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.my_tab, container, false);
+    }
+}
